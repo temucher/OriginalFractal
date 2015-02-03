75148cb0cc6d29adc65b2049597cff1639c37b80
temucher <tjsmuch@gmail.com>
2015-02-03 15:01:21 -0800

    first commit

--- build-tmp/source/OriginalFractal.java
+++ build-tmp/source/OriginalFractal.java
@@ -1,5 +1,21 @@
-private double fractionLength = .8; 
-private double branchAngle = .6;  
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class OriginalFractal extends PApplet {
+
+private double fractionLength = .8f; 
+private double branchAngle = .6f;  
 public void setup() {
 	size(1000,1000);
 }
@@ -63,3 +79,12 @@ public void drawFractal(int x, int y, double branchLength, double angle) {
 3.Add leaves to the stems, using drawLeaves().
 4.Add another stem to the first main stem, but make it shorter a shorter length
 */
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "OriginalFractal" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
+}
